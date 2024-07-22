f465c6292f4c994363beb710a3b06d4c405a78a1
prisVegaTest <priscilavegadelgado@outlook.com>
2024-07-22 10:49:07 -0500

    optimizartion-code
    se agrega cambio de optimización en código

--- cypress/e2e/ejercicio1.cy.js
+++ cypress/e2e/ejercicio1.cy.js
@@ -1,41 +1,40 @@
 describe('Purchase flow on Demoblaze', () => {
-    it('should complete a purchase flow successfully', () => {
-      cy.visit('https://www.demoblaze.com/');
-  
-      // Add the first product to the cart
-      cy.contains('Samsung galaxy s6').click();
-      cy.contains('Add to cart').click();
-      cy.on('window:alert', (text) => {
-        expect(text).to.contains('Product added');
-      });
-      cy.go('back');
-  
-      // Esperar a que el botón de home sea visible
-      cy.contains('Home').click();
-  
-      // Add the second product to the cart
-      cy.contains('Nokia lumia 1520').should('be.visible').click();
-      cy.contains('Add to cart').click();
-      cy.on('window:alert', (text) => {
-        expect(text).to.contains('Product added');
-      });
-      cy.go('back');
-  
-      // View the cart
-      cy.contains('Cart').click();
-  
-      // Complete the purchase form
-      cy.contains('Place Order').click();
-      cy.get('#name').type('Priscila Vega');
-      cy.get('#country').type('Ecuador');
-      cy.get('#city').type('Quito');
-      cy.get('#card').type('5556 4433 1111 0012');
-      cy.get('#month').type('02');
-      cy.get('#year').type('2029');
-      cy.contains('Purchase').click();
-  
-      // Verify the purchase was successful
-      cy.contains('Thank you for your purchase!').should('be.visible');
-      cy.contains('OK').click();
+  const addProductToCart = (productName) => {
+    cy.contains(productName).click();
+    cy.contains('Add to cart').click();
+    cy.on('window:alert', (text) => {
+      expect(text).to.contains('Product added');
     });
-  });  
+    cy.go('back');
+    cy.contains('Home').click();
+  };
+
+  const fillPurchaseForm = (name, country, city, card, month, year) => {
+    cy.get('#name').type(name);
+    cy.get('#country').type(country);
+    cy.get('#city').type(city);
+    cy.get('#card').type(card);
+    cy.get('#month').type(month);
+    cy.get('#year').type(year);
+    cy.contains('Purchase').click();
+  };
+
+  it('should complete a purchase flow successfully', () => {
+    cy.visit('https://www.demoblaze.com/');
+
+    // Add products to the cart
+    addProductToCart('Samsung galaxy s6');
+    addProductToCart('Nokia lumia 1520');
+
+    // View the cart
+    cy.contains('Cart').click();
+
+    // Complete the purchase form
+    cy.contains('Place Order').click();
+    fillPurchaseForm('Priscila Vega', 'Ecuador', 'Quito', '5556 4433 1111 0012', '02', '2029');
+
+    // Verify the purchase was successful
+    cy.contains('Thank you for your purchase!').should('be.visible');
+    cy.contains('OK').click();
+  });
+});
--- cypress/e2e/ejercicioApi.cy.js
+++ cypress/e2e/ejercicioApi.cy.js
@@ -1,142 +1,120 @@
 describe('Pruebas End-to-End para Demoblaze', () => {
-    const baseUrl = 'https://www.demoblaze.com';
-    const username = `testuser_${new Date().getTime()}`;
-    const password = 'testpassword';
-  
-    it('debería registrar un nuevo usuario a través de la API', () => {
-        const username = `testuser_${new Date().getTime()}`;
-        const password = 'testpassword';
-      
-        cy.request({
-          method: 'POST',
-          url: 'https://api.demoblaze.com/signup',
-          body: {
-            username: username,
-            password: password,
-          },
-          failOnStatusCode: false
-        }).then((response) => {
-          console.log(response); // Imprime toda la respuesta
-          expect(response.status).to.eq(200); // Verifica el código de estado
-          
-          // Verifica que el cuerpo tenga contenido si no está vacío
-          if (response.body) {
-            expect(response.body).to.not.be.empty;
-          } else {
-            cy.log('La respuesta está vacía');
-          }
-        });
-      });
-  
-    it('debería iniciar sesión a través de la API', () => {
-        const username = `testuser_${new Date().getTime()}`;
-        const password = 'testpassword';
-      
-        // Primero, registra al usuario
-        cy.request({
-          method: 'POST',
-          url: 'https://api.demoblaze.com/signup',
-          body: {
-            username: username,
-            password: password,
-          },
-          failOnStatusCode: false
-        }).then((signupResponse) => {
-          expect(signupResponse.status).to.eq(200);
-      
-          // Luego, inicia sesión
-          cy.request({
-            method: 'POST',
-            url: 'https://api.demoblaze.com/login',
-            body: {
-              username: username,
-              password: password,
-            },
-            failOnStatusCode: false
-          }).then((loginResponse) => {
-            console.log(loginResponse); // Imprime toda la respuesta
-            expect(loginResponse.status).to.eq(200); // Verifica el código de estado
-      
-            // Verifica que el cuerpo tenga contenido
-            if (loginResponse.body) {
-              expect(loginResponse.body).to.not.be.empty;
-            } else {
-              cy.log('La respuesta está vacía');
-            }
-          });
-        });
-      });
-  
-    it('deberia iniciar sesión exitoso del usuario registrado sin la API', () => {
-      cy.visit(baseUrl);
+  const baseUrl = 'https://www.demoblaze.com';
+  const apiUrl = 'https://api.demoblaze.com';
+  const username = `testuser_${new Date().getTime()}`;
+  const password = 'testpassword';
 
-      cy.get('#login2').contains('Log in').click({force: true});
-      cy.get('#logInModal > .modal-dialog > .modal-content').should('be.visible');
-
-      cy.get('#loginusername').type(username);
-      cy.get('#loginpassword').type(password);
-      cy.get('#logInModal > .modal-dialog > .modal-content > .modal-footer > .btn-primary').click({ force: true });
+  const registerUser = (username, password) => {
+    return cy.request({
+      method: 'POST',
+      url: `${apiUrl}/signup`,
+      body: {
+        username: username,
+        password: password,
+      },
+      failOnStatusCode: false
     });
+  };
 
-    it('deberia iniciar sesión del usuario con clave incorrecta registrado sin la API', () => {
-      cy.visit(baseUrl);
-
-      cy.get('#login2').contains('Log in').click({force: true});
-      cy.get('#logInModal > .modal-dialog > .modal-content').should('be.visible');
-
-      cy.get('#loginusername').type(username);
-      cy.get('#loginpassword').type('pasword1234');
-      cy.get('#logInModal > .modal-dialog > .modal-content > .modal-footer > .btn-primary').click({ force: true });
-
-      cy.window().then((win) => {
-        cy.stub(win, 'alert').callsFake((text) => {
-          expect(text).to.eq('Wrong password.');
-        });
+  const loginUser = (username, password) => {
+    return cy.request({
+      method: 'POST',
+      url: `${apiUrl}/login`,
+      body: {
+        username: username,
+        password: password,
+      },
+      failOnStatusCode: false
     });
+  };
 
-    it('deberia registrar un nuevo usuario sin la API', () => {
-      cy.visit(baseUrl);
-
-      cy.get('#signin2').contains('Sign up').click({force: true});
-      cy.get('#signInModal').should('be.visible');
-
-      cy.get('#sign-username').type(username);
-      cy.get('#sign-password').type(password);
-      cy.get('#signInModal .btn-primary').click({ force: true });
-
-      cy.window().then((win) => {
-        cy.stub(win, 'alert').callsFake((text) => {
-          if (text === 'Sign up successful.') {
-            expect(text).to.eq('Sign up successful.');
-          } else if (text === 'This user already exist.') {
-            expect(text).to.eq('This user already exist.');
-          }
-        });
-      });
-  
-      cy.wait(1000);
-      cy.get('#signInModal > .modal-dialog > .modal-content > .modal-header > .close > span').click();
-    });
-
-    it('debería manejar el registro de un usuario existente', () => {
-      cy.visit(baseUrl);
-  
-      cy.get('#signin2').contains('Sign up').click({ force: true });
-      cy.get('#signInModal').should('be.visible');
-  
-      cy.get('#sign-username').type(username);
-      cy.get('#sign-password').type(password);
+  it('debería registrar un nuevo usuario a través de la API', () => {
+    registerUser(username, password).then((response) => {
+      console.log(response); // Imprime toda la respuesta
+      expect(response.status).to.eq(200); // Verifica el código de estado
       
-      cy.get('#signInModal .btn-primary').click({ force: true });
-  
-      cy.window().then((win) => {
-        cy.stub(win, 'alert').callsFake((text) => {
+      // Verifica que el cuerpo tenga contenido si no está vacío
+      if (response.body) {
+        expect(response.body).to.not.be.empty;
+      } else {
+        cy.log('La respuesta está vacía');
+      }
+    });
+  });
+
+  it('debería iniciar sesión a través de la API', () => {
+    registerUser(username, password).then((signupResponse) => {
+      expect(signupResponse.status).to.eq(200);
+
+      loginUser(username, password).then((loginResponse) => {
+        console.log(loginResponse); // Imprime toda la respuesta
+        expect(loginResponse.status).to.eq(200); // Verifica el código de estado
+
+        // Verifica que el cuerpo tenga contenido
+        if (loginResponse.body) {
+          expect(loginResponse.body).to.not.be.empty;
+        } else {
+          cy.log('La respuesta está vacía');
+        }
+      });
+    });
+  });
+
+  const uiLogin = (username, password) => {
+    cy.visit(baseUrl);
+    cy.get('#login2').contains('Log in').click({ force: true });
+    cy.get('#logInModal > .modal-dialog > .modal-content').should('be.visible');
+    cy.get('#loginusername').type(username);
+    cy.get('#loginpassword').type(password);
+    cy.get('#logInModal > .modal-dialog > .modal-content > .modal-footer > .btn-primary').click({ force: true });
+  };
+
+  it('debería iniciar sesión exitoso del usuario registrado sin la API', () => {
+    uiLogin(username, password);
+  });
+
+  it('debería iniciar sesión del usuario con clave incorrecta registrado sin la API', () => {
+    cy.visit(baseUrl);
+    cy.get('#login2').contains('Log in').click({ force: true });
+    cy.get('#logInModal > .modal-dialog > .modal-content').should('be.visible');
+    cy.get('#loginusername').type(username);
+    cy.get('#loginpassword').type('incorrectpassword');
+    cy.get('#logInModal > .modal-dialog > .modal-content > .modal-footer > .btn-primary').click({ force: true });
+
+    cy.window().then((win) => {
+      cy.stub(win, 'alert').callsFake((text) => {
+        expect(text).to.eq('Wrong password.');
+      });
+    });
+  });
+
+  const uiRegister = (username, password) => {
+    cy.visit(baseUrl);
+    cy.get('#signin2').contains('Sign up').click({ force: true });
+    cy.get('#signInModal').should('be.visible');
+    cy.get('#sign-username').type(username);
+    cy.get('#sign-password').type(password);
+    cy.get('#signInModal .btn-primary').click({ force: true });
+
+    cy.window().then((win) => {
+      cy.stub(win, 'alert').callsFake((text) => {
+        if (text === 'Sign up successful.') {
+          expect(text).to.eq('Sign up successful.');
+        } else if (text === 'This user already exist.') {
           expect(text).to.eq('This user already exist.');
-        });
+        }
       });
-  
-      cy.wait(1000);
-      cy.get('#signInModal > .modal-dialog > .modal-content > .modal-header > .close > span').click();
     });
-    });
+
+    cy.wait(1000);
+    cy.get('#signInModal > .modal-dialog > .modal-content > .modal-header > .close > span').click();
+  };
+
+  it('debería registrar un nuevo usuario sin la API', () => {
+    uiRegister(username, password);
+  });
+
+  it('debería manejar el registro de un usuario existente', () => {
+    uiRegister(username, password);
+  });
 });
